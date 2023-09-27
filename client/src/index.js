a1c6a8c980d1c9f4ad6b349c81541067736f39cf
ktshglsm <ktshglsm35@gmail.com>
2023-09-27 11:03:09 +0700

    remove context

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { DarkModeContextProvider } from './context/darkModeContext';
-import { AuthContextProvider } from './context/authContext';
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
@@ -10,11 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <DarkModeContextProvider>
-        <AuthContextProvider >
-          <App />
-        </AuthContextProvider>
-      </DarkModeContextProvider>
+      <App />
     </PersistGate>
   </Provider>
 );
