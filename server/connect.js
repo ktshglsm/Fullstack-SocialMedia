2393eeb38678438960ba7ff8ae8e41e4c2b7eba4
ktshglsm <ktshglsm35@gmail.com>
2023-10-19 23:33:39 +0700

    server: add dotenv

--- server/connect.js
+++ server/connect.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require("dotenv")
 
+dotenv.config();
 
 const sequelize = new Sequelize({
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: '123456',
-    database: 'social',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     logging: false,
 });
-
-
 module.exports = sequelize;
